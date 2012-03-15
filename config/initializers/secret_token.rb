464fd4c5c84c443bcba6af4c52879cfeb5449483
Rick Duggan <rduggan@samsarmy7.(none)>
2012-03-14 23:13:00 -0700

    changed to pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyWatch::Application.config.secret_token = '089bd06f0632204c981f43cdd10c87a5cb784b60e42a4ad2a1b660ebb1d0006a42f6aebcf2136f05736e265c12da69ef2bddf4b2d1111a2a4cbb8dfdbfca4523'
